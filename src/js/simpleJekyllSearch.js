50fcf9bef27c7fe9b68a56e5ddfcfdc126cc1ade
erik <erik@dolins.ky>
2019-05-12 07:49:56 -0700

    fix series, format js, background colour

--- src/js/simpleJekyllSearch.js
+++ src/js/simpleJekyllSearch.js
@@ -1,13 +1,13 @@
-(function($) {
-    $.fn.simpleJekyllSearch = function(options) {
+(function ($) {
+    $.fn.simpleJekyllSearch = function (options) {
         var settings = $.extend({
-            jsonFile        : '/search.json',
-            jsonFormat      : 'title,category,desc,url,date,shortdate',
-            template : '<li><article><a href="{url}">{title} <span class="entry-date"><time datetime="{date}">{date}</time></span></a></article></li>',
-            searchResults   : '.search-results',
-            searchResultsTitle   : '<h4>Search Results:</h4>',
-            limit           : '5',
-            noResults       : '<p>Oh snap!<br/><small>Nothing found! =(</small></p>'
+            jsonFile: '/search.json',
+            jsonFormat: 'title,category,desc,url,date,shortdate',
+            template: '<li><article><a href="{url}">{title} <span class="entry-date"><time datetime="{date}">{date}</time></span></a></article></li>',
+            searchResults: '.search-results',
+            searchResultsTitle: '<h4>Search Results:</h4>',
+            limit: '5',
+            noResults: '<p>Oh snap!<br/><small>Nothing found! =(</small></p>'
         }, options);
 
         var properties = settings.jsonFormat.split(',');
@@ -16,16 +16,16 @@
             origThis = this,
             searchResults = $(settings.searchResults);
 
-        if(settings.jsonFile.length && searchResults.length){
+        if (settings.jsonFile.length && searchResults.length) {
             $.ajax({
                 type: "GET",
                 url: settings.jsonFile,
                 dataType: 'json',
-                success: function(data, textStatus, jqXHR) {
+                success: function (data, textStatus, jqXHR) {
                     jsonData = data;
                     registerEvent();
                 },
-                error: function(x,y,z) {
+                error: function (x, y, z) {
                     console.log("***ERROR in simpleJekyllSearch.js***");
                     console.log(x);
                     console.log(y);
@@ -36,53 +36,56 @@
         }
 
 
-        function registerEvent(){
-            origThis.keyup(function(e){
-                if($(this).val().length){
-                    writeMatches( performSearch($(this).val()) );
-                }else{
+        function registerEvent() {
+            origThis.keyup(function (e) {
+                if ($(this).val().length) {
+                    writeMatches(performSearch($(this).val()));
+                } else {
                     clearSearchResults();
                 }
             });
         }
 
-        function performSearch(str){
+        function performSearch(str) {
             var matches = [];
 
-            $.each(jsonData,function(i,entry){
-                for(var i=0;i<properties.length;i++)
-                    if(entry[properties[i]] !== undefined && entry[properties[i]].toLowerCase().indexOf(str.toLowerCase()) !== -1){
+            $.each(jsonData, function (i, entry) {
+                for (var i = 0; i < properties.length; i++)
+                    if (entry[properties[i]] !== undefined &&
+                        entry[properties[i]].toLowerCase()
+                            .indexOf(str.toLowerCase()) !== -1) {
                         matches.push(entry);
-                        i=properties.length;
+                        i = properties.length;
                     }
             });
             return matches;
 
         }
 
-        function writeMatches(m){
+        function writeMatches(m) {
             clearSearchResults();
-            searchResults.append( $(settings.searchResultsTitle) );
+            searchResults.append($(settings.searchResultsTitle));
 
-            if(m.length){
-                $.each(m,function(i,entry){
-                    if(i<settings.limit){
-                        var output=settings.template;
-                        for(var i=0;i<properties.length;i++){
-                            var regex = new RegExp("\{" + properties[i] + "\}", 'g');
+            if (m.length) {
+                $.each(m, function (i, entry) {
+                    if (i < settings.limit) {
+                        var output = settings.template;
+                        for (var i = 0; i < properties.length; i++) {
+                            var regex = new RegExp(
+                                "\{" + properties[i] + "\}", 'g');
                             output = output.replace(regex, entry[properties[i]]);
                         }
                         searchResults.append($(output));
                     }
                 });
-            }else{
-                searchResults.append( settings.noResults );
+            } else {
+                searchResults.append(settings.noResults);
             }
 
 
         }
 
-        function clearSearchResults(){
+        function clearSearchResults() {
             searchResults.children().remove();
         }
     }
